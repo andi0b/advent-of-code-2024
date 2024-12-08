359961a3c2993e77e9c5967aec62cb1f32aa16e1
andi0b <andix25@gmail.com>
2024-12-08 21:41:11 +0100

    Day07

--- Day07.fs
+++ Day07.fs
@@ -22,13 +22,11 @@ let findSolutions ops (expected, numbers) =
     ops
     |> allCombinations (List.length numbers - 1)
     |> Seq.filter (fun ops ->
-        let rec fold =
-            function
-            | acc, number :: remainingNumbers, op :: remainingOps ->
-                fold (op acc number, remainingNumbers, remainingOps)
-            | acc, _, _ -> acc
+        let result =
+            (numbers.Head, numbers.Tail, ops)
+            |||> List.fold2 (fun acc number op -> op acc number)
 
-        expected = fold (numbers.Head, numbers.Tail, ops))
+        result = expected)
 
 let solve ops input =
     input
@@ -70,18 +68,23 @@ module tests =
     let findSolutionCount ops equation =
         findSolutions ops equation |> Seq.length
 
-    [<Theory>]
-    [<InlineData("190: 10 19", 1)>]
-    [<InlineData("3267: 81 40 27", 2)>]
-    [<InlineData("292: 11 6 16 20", 1)>]
-    [<InlineData("21037: 9 7 18 13", 0)>]
+    let data1 =
+        [ "190: 10 19", 1
+          "3267: 81 40 27", 2
+          "292: 11 6 16 20", 1
+          "21037: 9 7 18 13", 0 ]
+
+    [<Theory; MemberData(nameof data1)>]
     let ``Part 1 solution count`` line count =
         line |> parseLine |> findSolutionCount [| (+); (*) |] =! count
 
-    [<Theory>]
-    [<InlineData("156: 15 6", 1)>]
-    [<InlineData("7290: 6 8 6 15", 1)>]
-    [<InlineData("192: 17 8 14", 1)>]
-    [<InlineData("21037: 9 7 18 13", 0)>]
+
+    let data2 =
+        [ "156: 15 6", 1
+          "7290: 6 8 6 15", 1
+          "192: 17 8 14", 1
+          "21037: 9 7 18 13", 0 ]
+
+    [<Theory; MemberData(nameof data2)>]
     let ``Part 2 solution count`` line count =
         line |> parseLine |> findSolutionCount [| (+); (*); (.||) |] =! count
