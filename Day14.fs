34ed0b124046795ce7a97e4698e5f8f61661defe
andi0b <andix25@gmail.com>
2024-12-15 17:41:18 +0100

    Day 14

--- Day14.fs
+++ Day14.fs
@@ -3,16 +3,15 @@ module aoc24.Day14
 open FSharp.Text.RegexProvider
 open FSharp.Text.RegexExtensions
 
-type Point = (struct (int64 * int64))
+type Point = (struct (int * int))
 type Robot = { loc: Point; vec: Point }
-
 
 module Point =
     let inline op f ((a, b): Point) ((c, d): Point) = Point(f a c, f b d)
 
 let inline (.+) a b = Point.op (+) a b
-let inline (.-) a b = Point.op (-) a b
 let inline (.%) a b = Point.op (%) a b
+let inline (.*) a b = Point.op (*) a b
 let inline (..*) ((x, y): Point) i = Point(x * i, y * i)
 let inline (../) ((x, y): Point) i = Point(x / i, y / i)
 
@@ -22,14 +21,14 @@ module Robot =
     let parse =
         R().TypedMatch
         >> (fun m ->
-            { loc = Point(m.px.AsInt64, m.py.AsInt64)
-              vec = Point(m.vx.AsInt64, m.vy.AsInt64) })
+            { loc = Point(m.px.AsInt, m.py.AsInt)
+              vec = Point(m.vx.AsInt, m.vy.AsInt) })
 
     let parseMany = Array.map parse
 
     let move (bounds: Point) times robot =
-        let result = robot.loc .+ (robot.vec .+ bounds) ..* times
-        result .% bounds
+        (robot.loc .+ ((robot.vec .+ bounds) .* (Point(times, times) .% bounds)))
+        .% bounds
 
     let quadrantSelector bounds =
         let ops = [ (>); (<) ]
@@ -37,7 +36,7 @@ module Robot =
         let selectors =
             List.allPairs ops ops
             |> List.mapi (fun i (xop, yop) ->
-                let struct (xBound, yBound) = bounds ../ 2L
+                let struct (xBound, yBound) = bounds ../ 2
                 fun ((x, y): Point) -> xop x xBound && yop y yBound)
 
         fun (p: Point) -> selectors |> List.tryFindIndex (fun s -> s p)
@@ -56,17 +55,18 @@ let solve1 bounds input =
     |> Array.reduce (*)
 
 let part1 input =
-    let bounds = Point(101L, 103L)
+    let bounds = Point(101, 103)
     solve1 bounds input
 
 let prettyPrint pos bounds =
     seq {
         yield System.Environment.NewLine
+
         for l = 0 to (bounds |> vsnd |> int) / 3 do
             for c = 0 to (bounds |> vfst |> int) do
 
                 let co offset =
-                    pos |> Set.contains (Point(c, 3 * l + offset |> int64))
+                    pos |> Set.contains (Point(c, 3 * l + offset))
 
                 yield
                     match [ co 0; co 1; co 2 ] with
@@ -88,7 +88,7 @@ let prettyPrint pos bounds =
 open FSharp.Stats
 
 let part2 input =
-    let bounds = Point(101L, 103L)
+    let bounds = Point(101, 103)
     let robots = input |> Robot.parseMany
 
     let varianceSums =
@@ -132,29 +132,29 @@ module tests =
            "p=9,5 v=-3,-3" |]
 
     [<Fact>]
-    let ``Part 1 example`` () = solve1 (Point(11L, 7L)) example1 =! 12
+    let ``Part 1 example`` () = solve1 (Point(11, 7)) example1 =! 12
 
     [<Fact>]
     let ``parse example`` () =
         let parsed = example1 |> Robot.parseMany
 
         parsed[0]
-        =! { loc = Point(0L, 4L)
-             vec = Point(3L, -3L) }
+        =! { loc = Point(0, 4)
+             vec = Point(3, -3) }
 
         parsed[1]
-        =! { loc = Point(6L, 3L)
-             vec = Point(-1L, -3L) }
+        =! { loc = Point(6, 3)
+             vec = Point(-1, -3) }
 
     [<Fact>]
     let ``move`` () =
         let robot = "p=2,4 v=2,-3" |> Robot.parse
-        let move = Robot.move (Point(11L, 7L))
+        let move = Robot.move (Point(11, 7))
 
-        test <@ robot |> move 1 = Point(4L, 1L) @>
-        test <@ robot |> move 2 = Point(6L, 5L) @>
-        test <@ robot |> move 3 = Point(8L, 2L) @>
-        test <@ robot |> move 4 = Point(10L, 6L) @>
-        test <@ robot |> move 5 = Point(1L, 3L) @>
+        test <@ robot |> move 1 = Point(4, 1) @>
+        test <@ robot |> move 2 = Point(6, 5) @>
+        test <@ robot |> move 3 = Point(8, 2) @>
+        test <@ robot |> move 4 = Point(10, 6) @>
+        test <@ robot |> move 5 = Point(1, 3) @>
 
         ()
